91394a2880cf8621e53ce030ced2e0c4319734dc
srinivas <chokkallasrinivas@gmail.com>
2022-03-08 15:49:56 +0530

    done changes

---
+++ programe1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TextcomponentComponent,
+    ImagecomponentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
